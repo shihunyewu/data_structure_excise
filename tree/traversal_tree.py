97337d2e964fd23cf08dd0581170bab9e0485795
shihunyewu <sgy_qingdao2016@163.com>
2018-03-25 00:06:34 +0800

    更新树遍历算法

--- tree/traversal_tree.py
+++ tree/traversal_tree.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+
 # 使用 7 种方式遍历二叉树
 # 递归：前序，中序，后序
 # 非递归：前序，中序，后序，层次
@@ -50,25 +53,72 @@ def last(node):
 
 # 无递归先序遍历
 def pre_no_recursion(node):
+    if node == None:
+        return
+    s = []
+    while s or node:
+        while node:
+            print(node.val)
+            s.append(node)
+            node = node.left
+        node = s.pop()
+        node = node.right
     pass
 
 # 无递归中序遍历
+# 左 根 右
 def mid_no_recursion(node):
+    if node == None:
+        return
+    s = []
+    while s or node:
+        while node:
+            s.append(node)
+            node = node.left
+        node = s.pop()
+        print(node.val)
+        node = node.right
     pass
 
 # 无递归后序遍历
 def last_no_recursion(node):
+    if node == None:
+        return
+    s = []
+    rs = []
+    s.append(node)
+    while s:
+        node = s.pop()
+        # 从宏观上来看，具有同父结点的左右结点，右子树的结点最早进入栈中
+        if node.left:
+            s.append(node.left)
+        if node.right:
+            s.append(node.right)
+        rs.append(node)
+    while rs:
+        print(rs.pop().val)
+    # 假如不使用倒序呢？
     pass
 
 # 层次遍历
 def level_traversal(node):
+
     pass
 
-tree_list = [1,5,2]
+tree_list = [1,2,3,4,5,6,7]
 node = construct_tree(tree_list,0)
-print('先序遍历：')
+print '先序遍历：'
 pre(node)
 print('中序遍历')
 mid(node)
 print('后序遍历')
 last(node)
+print('非递归前序遍历')
+pre_no_recursion(node)
+print('非递归中序遍历')
+mid_no_recursion(node)
+print('非递归后序遍历')
+last_no_recursion(node)
+
+s = [10,11]
+s.pop(0)# s.pop(0) 就是删除第一个元素，相当于出队
