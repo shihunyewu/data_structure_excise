ae934c0da538355c6a8bd472a9a31cf657ec991e
shihunyewu <sgy_qingdao2014@163.com>
2018-05-03 09:35:58 +0800

    更新文件名，更新README

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 > 数据结构练习
 #### 文件描述
 * graph:图相关
-    * dfs.py  
+    * dfs
       使用深度优先，在不连通图中找最大连通分支，使用递归和非递归两种方式实现
+    * shortestPath
+      最短路径
 * tree:树相关
-    * traverse_tree  
+    * traverseTree
       分别使用递归和非递归方式进行先序，中序，后序遍历，另外还实现了层次遍历
+    * segmentTree
+    线段树
---
+++ tree/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding = utf-8 -*-
+"""
+687. Longest Univalue Path
+"""
+
+
+# [5,4,5,1,1,5]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
